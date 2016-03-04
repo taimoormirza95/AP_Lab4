80366c23f791bbdb7786c6b8759d79466889802d
taimoormirza95 <13bscsmbaig@seecs.edu.pk>
2016-03-04 16:21:24 +0500

    Create recc.java

---
+++ recc.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spidercrawler;
+
+
+import java.io.*;     
+import java.util.*; 
+
+public class Recc {
+		public int FileSize;
+		public String FileName;
+		public String FilePath;
+    
+		
+}
