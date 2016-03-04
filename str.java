4551e74a314ff31adbf1d7dd30b1de1cfadca0ac
taimoormirza95 <13bscsmbaig@seecs.edu.pk>
2016-03-04 16:21:05 +0500

    Create str.java

---
+++ str.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spidercrawler;
+
+import java.io.*;     
+import java.util.*; 
+
+public class Str
+{
+	public String FileName;
+	public String FilePath;
+	public String FileSize;
+    
+	
+}
